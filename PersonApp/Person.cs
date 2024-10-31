5e6ea6884de84995e4290924fd635e7a9c127a74
CHZelaya <chzelaya1201@gmail.com>
2024-10-30 20:21:29 -0600

    Add project files.

---
+++ PersonApp/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    internal class Person
+    {
+        // attributes
+        private string fname;
+        private string lname;
+        private string dob;
+
+
+        // get and set methods for the attributes
+        public string Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+
+        public string Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+
+        private string Dob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        // constructor for the Person class
+        public Person(string fname, string lname, string dob)
+        {
+            Fname = fname;
+            Lname = lname;
+            Dob = dob;
+        }
+
+        // Name cannot be null and will be "Unknown" if it is not provided by the user
+        public string Name
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(fname) && string.IsNullOrWhiteSpace(lname)
+                    ? "Unknown"
+                    : $"{fname} {lname}";
+            }
+        }
+
+        // Dob should not be in the future and not more than 125 years ago.
+
+        public string Age
+        {
+            get
+            {
+                DateTime parsedDate; // DateTime is the datatype, parsedDate is the declared variable
+
+                // Try to parse the date
+
+                // For my own understanding, dob is the input string, out indicates that it is an output parameter, if parsing is successfull, it is stored in (OUT TO) parsedDate which NEEDs to be declared previously, as on line 61
+
+
+                if (DateTime.TryParse(dob, out parsedDate))
+                {
+                    // check if the date is in the future
+                    if (parsedDate > DateTime.Now)
+                    {
+                        return "The birthdate cannot be in the future";
+                    }
+                    // Check to see if the date is over 125 years old
+                    else if (parsedDate < DateTime.Now.AddYears(-125))
+                    {
+                        return "The birthdate is over 125 years old";
+                    }
+                    else
+                    {
+                        // Calculate age
+                        int age = DateTime.Now.Year - parsedDate.Year;
+                        if (DateTime.Now < parsedDate.AddYears(age)) age--; // Adjust for birthday
+                        return age.ToString();
+                    }
+                }
+                else
+                {
+                    return "Invalid date format.";
+                }
+            }
+
+        }
+
+
+        //Override ToString Method
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+
+    }
+}
---
+++ PersonApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Person person1 = new Person("Evil", "Morty", "2025-05-15");
+            Person person2 = new Person("Rick", "Sanchez", "1990-05-15");
+            Person person3 = new Person("Bird", "Person", "1889-10-31");
+            Person person4 = new Person(" ", " ", "01/01/2000");
+
+            //Testing to see if the date can be in different formats
+            Person person5 = new Person("Morty", "Smith", "09/19/2002");
+
+            Console.WriteLine(person1);
+            Console.WriteLine(person2);
+            Console.WriteLine(person3);
+            Console.WriteLine(person4);
+            Console.WriteLine(person5);
+
+        }
+
+
+    }
+}
